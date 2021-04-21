b97fbbecb614fa247f45d4d5ab0cb40e27542db9
Vít Skalický <vit.skalicky@email.cz>
2021-04-21 23:04:33 +0200

    converted import theme fragment to kotlin

--- app/src/main/java/cz/vitskalicky/lepsirozvrh/settings/ImportThemeFragment.kt
+++ app/src/main/java/cz/vitskalicky/lepsirozvrh/settings/ImportThemeFragment.kt
@@ -1,172 +1,153 @@
-package cz.vitskalicky.lepsirozvrh.settings;
+package cz.vitskalicky.lepsirozvrh.settings
 
-import android.app.Activity;
-import android.content.ClipboardManager;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.text.method.LinkMovementMethod;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import java.io.IOException;
-
-import cz.vitskalicky.lepsirozvrh.R;
-import cz.vitskalicky.lepsirozvrh.SharedPrefs;
-import cz.vitskalicky.lepsirozvrh.Utils;
-import cz.vitskalicky.lepsirozvrh.donations.Donations;
-import cz.vitskalicky.lepsirozvrh.theme.Theme;
-import cz.vitskalicky.lepsirozvrh.theme.ThemeData;
-
-import static android.content.Context.CLIPBOARD_SERVICE;
+import android.app.Activity
+import android.content.ClipboardManager
+import android.content.Context
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import cz.vitskalicky.lepsirozvrh.R
+import cz.vitskalicky.lepsirozvrh.SharedPrefs
+import cz.vitskalicky.lepsirozvrh.Utils.RecreateWithAnimationActivity
+import cz.vitskalicky.lepsirozvrh.donations.Donations
+import cz.vitskalicky.lepsirozvrh.theme.Theme
+import cz.vitskalicky.lepsirozvrh.theme.ThemeData
+import java.io.IOException
 
 /**
- * A simple {@link Fragment} subclass.
+ * A simple [Fragment] subclass.
  */
-public class ImportThemeFragment extends Fragment {
-
-    private Donations donations;
-
-    private View root;
-    private EditText editTextData;
-    private String preloadedString = "";
-    private Button buttonPaste;
-    private Button buttonOK;
-    private Button buttonClear;
-    private TextView tvInfo;
-    public ImportThemeFragment() {
-        // Required empty public constructor
+class ImportThemeFragment : Fragment() {
+    private var donations: Donations? = null
+    private var root: View? = null
+    private var editTextData: EditText? = null
+    private var preloadedString = ""
+    private var buttonPaste: Button? = null
+    private var buttonOK: Button? = null
+    private var buttonClear: Button? = null
+    private var tvInfo: TextView? = null
+    fun init(donations: Donations?) {
+        this.donations = donations
+        updateDonationsStatus()
     }
 
-    public void init(Donations donations){
-        this.donations = donations;
-        updateDonationsStatus();
-    }
-
-    public void setString(String preloadedString) {
-        this.preloadedString = preloadedString;
+    fun setString(preloadedString: String) {
+        this.preloadedString = preloadedString
         if (editTextData != null) {
-            editTextData.setText(preloadedString);
+            editTextData!!.setText(preloadedString)
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         if (root == null) {
-            root = inflater.inflate(R.layout.fragment_import_theme, container, false);
-            editTextData = root.findViewById(R.id.editTextData);
-            buttonPaste = root.findViewById(R.id.buttonPaste);
-            buttonOK = root.findViewById(R.id.buttonOK);
-            buttonClear = root.findViewById(R.id.buttonClear);
-            tvInfo = root.findViewById(R.id.textViewInfo);
+            root = inflater.inflate(R.layout.fragment_import_theme, container, false)
+            editTextData = root!!.findViewById(R.id.editTextData)
+            buttonPaste = root!!.findViewById(R.id.buttonPaste)
+            buttonOK = root!!.findViewById(R.id.buttonOK)
+            buttonClear = root!!.findViewById(R.id.buttonClear)
+            tvInfo = root!!.findViewById(R.id.textViewInfo)
         }
-
-        tvInfo.setMovementMethod(LinkMovementMethod.getInstance());
-
-        buttonPaste.setOnClickListener(v -> {
-            ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(CLIPBOARD_SERVICE);
-            if (clipboard.getPrimaryClip() != null && clipboard.getPrimaryClip().getItemCount() > 0) {
-                editTextData.setText(clipboard.getPrimaryClip().getItemAt(0).getText());
+        tvInfo!!.movementMethod = LinkMovementMethod.getInstance()
+        buttonPaste!!.setOnClickListener { v: View? ->
+            val clipboard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            if (clipboard.primaryClip != null && clipboard.primaryClip!!.itemCount > 0) {
+                editTextData!!.setText(clipboard.primaryClip!!.getItemAt(0).text)
             }
-        });
-        buttonOK.setOnClickListener(this::doImport);
-        buttonClear.setOnClickListener(v -> editTextData.setText(""));
-        updateDonationsStatus();
-
-        return root;
+        }
+        buttonOK!!.setOnClickListener { v: View -> doImport(v) }
+        buttonClear!!.setOnClickListener { v: View? -> editTextData!!.setText("") }
+        updateDonationsStatus()
+        return root
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (!preloadedString.isEmpty()){
-            editTextData.setText(preloadedString);
-            preloadedString = "";
+    override fun onResume() {
+        super.onResume()
+        if (!preloadedString.isEmpty()) {
+            editTextData!!.setText(preloadedString)
+            preloadedString = ""
         }
     }
 
-    private void doImport(View v){
-        AsyncTask.execute(() -> {
-            ThemeData td = null;
-            String original = editTextData.getText().toString().replaceAll("\\s",""); //remove all whitespaces
-            String input = original;
-            if (input.startsWith("https://vitskalicky.github.io/lepsi-rozvrh/motiv-info") || input.startsWith("https://vitskalicky.gitlab.io/lepsi-rozvrh/motiv-info")){
-                Uri uri = Uri.parse(input);
-                input = uri.getQueryParameter("data");
-            } else if (input.startsWith("lepsi-rozvrh:motiv/")){
-                input = input.substring(19);
+    private fun doImport(v: View) {
+        AsyncTask.execute {
+            var td: ThemeData? = null
+            val original = editTextData!!.text.toString().replace("\\s".toRegex(), "") //remove all whitespaces
+            var input = original
+            if (input.startsWith("https://vitskalicky.github.io/lepsi-rozvrh/motiv-info") || input.startsWith("https://vitskalicky.gitlab.io/lepsi-rozvrh/motiv-info")) {
+                val uri = Uri.parse(input)
+                input = uri.getQueryParameter("data") ?: ""
+            } else if (input.startsWith("lepsi-rozvrh:motiv/")) {
+                input = input.substring(19)
             }
-            if (input == null || input.isEmpty()){
-                input = original;
+            if (input == null || input.isEmpty()) {
+                input = original
             }
             try {
-                td = ThemeData.parseZipped(input);
-            } catch (IOException e) {
+                td = ThemeData.parseZipped(input)
+            } catch (e: IOException) {
                 //try to parse as json (not zipped)
                 try {
-                    td = ThemeData.parseJson(input);
-                } catch (IOException ex) {
+                    td = ThemeData.parseJson(input)
+                } catch (ex: IOException) {
                     //try to parse as base64, but not url-safe
-                    input = input.replace('+','-').replace('/','_');
+                    input = input.replace('+', '-').replace('/', '_')
                     try {
-                        td = ThemeData.parseZipped(input);
-                    } catch (IOException exc) {
+                        td = ThemeData.parseZipped(input)
+                    } catch (exc: IOException) {
                         //try fixing the data (find the magic number (+compression method - always same) of gzip: H4s)
-                        try{
-                            int index = input.indexOf("H4s");
-                            if (index > -1){
-                                td = ThemeData.parseZipped(input.substring(index));
+                        try {
+                            val index = input.indexOf("H4s")
+                            if (index > -1) {
+                                td = ThemeData.parseZipped(input.substring(index))
                             }
-                        }catch (IOException ignored) {
+                        } catch (ignored: IOException) {
                         }
                     }
                 }
             }
-            final ThemeData ftd = td;
-            new Handler(Looper.getMainLooper()).post(() -> {
+            val ftd = td
+            Handler(Looper.getMainLooper()).post {
                 if (ftd != null) {
-                    Theme.of(getContext()).setThemeData(ftd);
-                    SharedPrefs.setIntPreference(getContext(), R.string.PREFS_DETAIL_LEVEL, 3);
-                    SharedPrefs.setStringPreference(getContext(),R.string.PREFS_APP_THEME, "4");
-
-                    Activity activity = getActivity();
-                    if (activity instanceof Utils.RecreateWithAnimationActivity) {
-                        ((Utils.RecreateWithAnimationActivity) activity).recreateWithAnimation();
+                    Theme.of(context).themeData = ftd
+                    SharedPrefs.setIntPreference(context, R.string.PREFS_DETAIL_LEVEL, 3)
+                    SharedPrefs.setStringPreference(context, R.string.PREFS_APP_THEME, "4")
+                    val activity: Activity? = activity
+                    if (activity is RecreateWithAnimationActivity) {
+                        (activity as RecreateWithAnimationActivity).recreateWithAnimation()
                     }
                 } else {
-                    Snackbar s = Snackbar.make(root, R.string.import_invalid, Snackbar.LENGTH_LONG);
-                    s.show();
+                    val s = Snackbar.make(root!!, R.string.import_invalid, Snackbar.LENGTH_LONG)
+                    s.show()
                 }
-            });
-        });
-    }
-
-    public void updateDonationsStatus(){
-        if (donations != null && root != null){
-            buttonOK.setEnabled(donations.isSponsor());
-            if (donations.isSponsor()){
-                buttonOK.setOnClickListener(this::doImport);
-            }else {
-                buttonOK.setOnClickListener(v -> donations.showDialog());
             }
         }
     }
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
+    fun updateDonationsStatus() {
+        if (donations != null && root != null) {
+            buttonOK!!.isEnabled = donations!!.isSponsor
+            if (donations!!.isSponsor) {
+                buttonOK!!.setOnClickListener { v: View -> doImport(v) }
+            } else {
+                buttonOK!!.setOnClickListener { v: View? -> donations!!.showDialog() }
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
     }
 }
