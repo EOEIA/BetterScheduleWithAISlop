a72c6cb6983fd9cb8ed302f898946de3f5b0d930
Vít Skalický <vit.skalicky@email.cz>
2021-05-21 18:13:14 +0200

    Handle job cancellation exception

--- app/src/main/java/cz/vitskalicky/lepsirozvrh/schoolsDatabase/SchoolsListViewModel.kt
+++ app/src/main/java/cz/vitskalicky/lepsirozvrh/schoolsDatabase/SchoolsListViewModel.kt
@@ -11,6 +11,7 @@ import cz.vitskalicky.lepsirozvrh.R
 import cz.vitskalicky.lepsirozvrh.SharedPrefs
 import cz.vitskalicky.lepsirozvrh.model.StatusInfo
 import cz.vitskalicky.lepsirozvrh.model.StatusInfo.Specification
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 import okio.IOException
 import org.joda.time.DateTime
@@ -67,12 +68,14 @@ class SchoolsListViewModel(
                 statusLD.value = StatusInfo.error(Specification.ERROR_UNREACHABLE, R.string.no_internet)
             }
             null
-        }catch (e: HttpException){
+        }catch (e: HttpException) {
             statusLD.value = StatusInfo.error(Specification.ERROR_UNEXPECTED_RESPONSE, R.string.schools_info_connection_failed)
-            if (!KotlinUtils.isOnline()){
+            if (!KotlinUtils.isOnline()) {
                 statusLD.value = StatusInfo.error(Specification.ERROR_UNREACHABLE, R.string.no_internet)
             }
             null
+        }catch (e: CancellationException) {
+            throw e;
         }catch (e: Exception){
             val f = RuntimeException("Failed to load schools list: ${e.message}", e)
             app().sendReport(f)
