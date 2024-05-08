1dbdd37d79ad1407bdbb30af1e64af9bf46bd33d
Vít Skalický <vit.skalicky@email.cz>
2024-05-08 21:16:16 +0200

    Make SchoolInfo parcelable which fixed the issue with login screen

--- app/src/main/java/cz/vitskalicky/lepsirozvrh/schoolPicker/SchoolInfo.kt
+++ app/src/main/java/cz/vitskalicky/lepsirozvrh/schoolPicker/SchoolInfo.kt
@@ -1,11 +1,14 @@
 package cz.vitskalicky.lepsirozvrh.schoolPicker
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.Fts4
+import kotlinx.parcelize.Parcelize
 import java.text.Normalizer
 
 @Fts4
 @Entity(tableName = "schools")
+@Parcelize
 data class SchoolInfo(
         val id: String,
         val name: String,
@@ -15,7 +18,7 @@ data class SchoolInfo(
          * only letters and digits from 'name', lowercase (accents are removed)
          */
         val search_text: String? = "$name $url".simplified()
-)
+) : Parcelable
 
 /**
  * Removes all accents, converts punctuation to spaces, deletes all non-alphanumerical characters and removes duplicate spaces
